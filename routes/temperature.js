203f6036cf03fcddea9ea3283070cbcf727292a9
Tomasz Gałązka <tomasz.galazka@contractors.roche.com>
2019-01-29 10:15:27 +0100

    es5

--- routes/temperature.js
+++ routes/temperature.js
@@ -1,24 +1,23 @@
-var express = require('express');
-var fs = require('fs');
-var router = express.Router();
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
 
-var parseSensorData = function (tempText) {
-	var tConst = 't=';
-	if (tempText.indexOf(tConst) < 0){
+const parseSensorData = (tempText) => {
+	const tConst = 't=';
+	if (tempText.indexOf(tConst) < 0) {
 		return;
 	}
-	var temp1 = tempText.substring(tempText.indexOf(tConst) + 2, tempText.length);
-	return ((temp1/1000)-2);	 
+	const temp1 = tempText.substring(tempText.indexOf(tConst) + 2, tempText.length);
+	return '' + ((temp1 / 1000) - 2);
 }
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  fs.readFile('/sys/bus/w1/devices/28-0000095c9ffc/w1_slave', (err, data) => {
-    if (err) throw err;
-    
-    res.send(parseSensorData(data));
-  });
-  
+router.get('/', (req, res, next) => {
+	// fs.readFile('fake.teperature.txt', (err, data) => {
+	fs.readFile('/sys/bus/w1/devices/28-0000095c9ffc/w1_slave', (err, data) => {
+		if (err) throw err;
+		res.send(parseSensorData(data.toString()));
+	});
+
 });
 
 module.exports = router;
