357e03972bc6c52925c9bd36b9cd8e5d61efd794
Tomasz Gałązka <tomasz.galazka@contractors.roche.com>
2019-01-29 08:04:02 +0100

    temperature read

--- routes/temperature.js
+++ routes/temperature.js
@@ -2,12 +2,21 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
+var parseSensorData = function (tempText) {
+	var tConst = 't=';
+	if (tempText.indexOf(tConst) < 0){
+		return;
+	}
+	var temp1 = tempText.substring(tempText.indexOf(tConst) + 2, tempText.length);
+	return ((temp1/1000)-2);	 
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   fs.readFile('/sys/bus/w1/devices/28-0000095c9ffc/w1_slave', (err, data) => {
     if (err) throw err;
-    console.log(data);
-    res.send('respond: ' + data );
+    
+    res.send(parseSensorData(data));
   });
   
 });
