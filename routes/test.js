fb7f9158a68e42e219d4fb829d347af6decd5db6
Tomasz Gałązka <tomasz.galazka@itcraft.pl>
2022-03-10 08:11:48 +0100

    startup mode

--- routes/test.js
+++ routes/test.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-// const temperatureRead = require('../app/temperature-reader');
+const temperatureRead = require('../app/temperature-reader');
 
 router.get('/', async (req, res, next) => {
-	// const temperature = await temperatureRead();
-	// const text = `Temperatura: ${temperature.toFixed(2)} C`
-	// res.send(text);
-	 res.send('text');
+	const temperature = await temperatureRead();
+	const text = `Temperatura: ${temperature.toFixed(2)} C`
+	res.send(text);
 });
 
 module.exports = router;
